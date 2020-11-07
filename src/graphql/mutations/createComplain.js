eefb6c6865bd7c6c7e964b43d270ad0e595e014c
Nico <nicolasarias870@gmail.com>
2020-11-07 01:37:40 -0500

    createComplaint mutation

--- src/graphql/mutations/createComplain.js
+++ src/graphql/mutations/createComplain.js
@@ -1,30 +1,35 @@
-import { from } from "@apollo/client";
-import { gql} from '@apollo/client'
+import { gql } from "@apollo/client";
 
-export default createComplaint = gql`
-   mutation {
-  createComplaint(
-    	description: "dasdas",
-    	linkEvidence: "sadasddas",
-    	offeredPrice: 500,
-    	typeComplaint: "new complaint",
-    	priceId: 123213
-  ) {
-    complaint {
-      user {
-        username
-      }
-      station {
-        id
-      }
-      description
-      linkEvidence
-      typeComplaint
-      actualPrice {
-        id
-      }
-      offeredPrice
+export default gql`
+mutation createComplaint(
+        description: String!,
+        linkEvidence: String!,
+        offeredPrice: Int!,
+        typeComplaint: String!,
+        priceId: Int!
+    ) {
+    createComplaint(
+        description: $description,
+        linkEvidence: $linkEvidence,
+        offeredPrice: $offeredPrice,
+        typeComplaint: $typeComplaint,
+        priceId: $priceId
+    ) {
+        complaint {
+        user {
+            username
+        }
+        station {
+            id
+        }
+        description
+        linkEvidence
+        typeComplaint
+        actualPrice {
+            id
+        }
+        offeredPrice
+        }
     }
-  }
 }
 `;
